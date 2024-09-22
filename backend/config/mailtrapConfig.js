653e7cbfe1c4850327618d8243cd508071044de2
MAbdurahman <mdbdrrhm@gmail.com>
2024-09-22 07:06:26 -0500

    Add config/mailtrapConfig.js

---
+++ backend/config/mailtrapConfig.js
@@ -0,0 +1,33 @@
+// Looking to send emails in production? Check out our Email API/SMTP product!
+
+import {MailtrapClient} from 'mailtrap';
+import dotenv from 'dotenv';
+
+dotenv.config({path: 'backend/config/config.env'});
+
+export const mailtrapClient = new MailtrapClient({
+   endpoint: process.env.MAILTRAP_ENDPOINT,
+   token: process.env.MAILTRAP_TOKEN,
+   testInboxId: process.env.MAILTRAP_ID,
+});
+
+export const sender = {
+   email: "hello@example.com",
+   name: "MERN Auth Advance",
+};
+
+const recipients = [
+   {
+      email: process.env.MAILTRAP_RECIPIENT,
+   }
+];
+
+mailtrapClient.testing
+   .send({
+      from: sender,
+      to: recipients,
+      subject: "You are awesome!",
+      text: "Congrats for sending test email with Mailtrap!",
+      category: "Integration Test",
+   })
+   .then(console.log, console.error);
