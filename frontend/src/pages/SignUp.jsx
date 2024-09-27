ca50b30fd886b893806caa25b857184521980a7b
MAbdurahman <mdbdrrhm@gmail.com>
2024-09-26 19:01:34 -0500

    Begin pages/SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,22 @@
+/************************* imports *************************/
+import {motion} from 'framer-motion';
+
 export default function SignUp() {
 
    return (
-      <div>
-         <h2>SignUp</h2>
-      </div>
+      <motion.div
+         initial={{opacity: 0, y: 20}}
+         animate={{opacity: 1, y: 0}}
+         transition={{duration: 0.5}}
+         className='max-w-md w-full bg-neutral-200 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl
+			overflow-hidden'
+      >
+         <div className='p-8'>
+            <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-neutral-400 to-neutral-500 text-transparent bg-clip-text'>
+               Create Account
+            </h2>
+         </div>
+      </motion.div>
 
    );
 }
